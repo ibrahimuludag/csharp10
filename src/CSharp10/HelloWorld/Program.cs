b7a9222d8a48d135a1f157b1fecd647cd7f3808f
ibrahimuludag <ibrahimuludag@gmail.com>
2021-12-22 00:23:45 +0300

    Add HelloWorld Project

---
+++ src/CSharp10/HelloWorld/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("This console application is a top level program.");
+Console.WriteLine("There is no namespace, no class and no using.");
+Console.WriteLine("System namespace is imported implicitly.");
+
