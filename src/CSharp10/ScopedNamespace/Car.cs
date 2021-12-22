428f4f4ba03e3dfad0c7e29d09fca22c351aa571
ibrahimuludag <ibrahimuludag@gmail.com>
2021-12-22 18:02:15 +0300

    Add ScopedNamespace

---
+++ src/CSharp10/ScopedNamespace/Car.cs
@@ -0,0 +1,8 @@
+﻿// This is a file scoped namespace. You can have one per file.
+namespace ScopedNamespace;
+public class Car {
+    public string Make { get; set; }
+    public string Model { get; set; }
+    public int Year { get; set; }
+}
+
---
+++ src/CSharp10/ScopedNamespace/Program.cs
@@ -0,0 +1,9 @@
+﻿using ScopedNamespace;
+
+var car = new Car
+{
+    Make = "Ssangyong",
+    Model = "Korando",
+    Year = 2020
+};
+Console.WriteLine($"{car.Make}");
