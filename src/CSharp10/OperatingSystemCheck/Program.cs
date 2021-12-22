a560925028d3ce8267be0a180f6d4c5238033d40
ibrahimuludag <ibrahimuludag@gmail.com>
2021-12-22 16:24:27 +0300

    Check OS using symbols

--- src/CSharp10/OperatingSystemCheck/Program.cs
+++ src/CSharp10/OperatingSystemCheck/Program.cs
@@ -15,3 +15,13 @@ else if (OperatingSystem.IsBrowser())
     Console.WriteLine("You running on browser with a Blazor app");
 }
 
+
+#if NET6_0_OR_GREATER
+    Console.WriteLine("You running on .NET 6 or greater");
+#endif
+#if NET6_0_WINDOWS
+    Console.WriteLine("You running on .NET 6 or greater on Windows");
+#endif
+#if NET6_0_IOS
+    Console.WriteLine("You running on .NET 6 on IOS");
+#endif
