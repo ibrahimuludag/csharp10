47ef424870f9b91496cc5fe57b31504f575b043a
ibrahimuludag <ibrahimuludag@gmail.com>
2021-12-22 16:20:57 +0300

    Add OperatingSystemCheck

---
+++ src/CSharp10/OperatingSystemCheck/Program.cs
@@ -0,0 +1,17 @@
+﻿if (OperatingSystem.IsWindows())
+{
+    Console.WriteLine("You running on Windows");
+}
+else if (OperatingSystem.IsWindowsVersionAtLeast(major: 10))
+{
+    Console.WriteLine("You running on Windows 10 or later");
+}
+else if (OperatingSystem.IsIOSVersionAtLeast(major: 14, minor: 5))
+{
+    Console.WriteLine("You running on IOS at least 14.5");
+}
+else if (OperatingSystem.IsBrowser())
+{
+    Console.WriteLine("You running on browser with a Blazor app");
+}
+
