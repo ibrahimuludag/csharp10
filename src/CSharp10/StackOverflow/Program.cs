97622890e95e1600d9611f275ca36b0248c3205b
ibrahimuludag <ibrahimuludag@gmail.com>
2021-12-22 16:44:49 +0300

    Add OverflowException

---
+++ src/CSharp10/StackOverflow/Program.cs
@@ -0,0 +1,27 @@
+﻿using static System.Console;
+
+var bigNumber = int.MaxValue - 1;
+WriteLine($"Number is {bigNumber}");
+bigNumber++;
+WriteLine($"Number is {bigNumber}");
+bigNumber++;
+WriteLine($"Number is {bigNumber}");
+bigNumber++;
+WriteLine($"Number is {bigNumber}");
+WriteLine($"There is OverflowException but exception is not thrown becuase the code block is not surrounded with checked");
+WriteLine($"Below code block will throw OverflowException");
+
+checked
+{
+    var newBigNumber = int.MaxValue - 1;
+    WriteLine($"Number is {newBigNumber}");
+    newBigNumber++;
+    WriteLine($"Number is {newBigNumber}");
+    newBigNumber++;
+    WriteLine($"Number is {newBigNumber}");
+    newBigNumber++;
+    WriteLine($"Number is {newBigNumber}");
+}
+
+
+
