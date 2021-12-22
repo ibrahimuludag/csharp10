f81e3460e9240d48e43503ce78f153a8a3eab934
ibrahimuludag <ibrahimuludag@gmail.com>
2021-12-22 20:47:11 +0300

    Add nullable variable

---
+++ src/CSharp10/Nullable/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+// To enable nullable variable, it must be enables in the project config 
+// <Nullable>disable</Nullable>
+// or add #nullable disable
+#nullable disable
+string var1 = null;
+Console.WriteLine("Hello, World!");
---
+++ src/CSharp10/Nullable/README.md
@@ -0,0 +1,3 @@
+﻿ To enable nullable variable, it must be enables in the project config 
+ <Nullable>disable</Nullable>
+ or add #nullable disable
