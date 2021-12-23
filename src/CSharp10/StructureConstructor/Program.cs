1f0760f3b945cb1f9d2f6766b38cd9c9648f74ba
ibrahimuludag <ibrahimuludag@gmail.com>
2021-12-23 13:28:02 +0300

    Add Structure Constructor

---
+++ src/CSharp10/StructureConstructor/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using StructureConstructor;
+
+var point = new Point(); // Parameterless constructor for structures is new C# feature 
+point.X = 10;
+point.Y = 20;
+Console.WriteLine(point);
