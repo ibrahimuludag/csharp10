529696d1a9ef6b502163d720dde7e8b58f1b64de
ibrahimuludag <ibrahimuludag@gmail.com>
2021-12-23 11:44:43 +0300

    Add deconstruct

---
+++ src/CSharp10/Deconstruct/Person.cs
@@ -0,0 +1,14 @@
+﻿namespace Deconstruct;
+public class Person
+{
+    public string Name { get; set; }
+    public string LastName { get; set; }
+    public Person(string name, string lastname) =>
+        (Name, LastName) = (name, lastname);
+
+    public void Deconstruct(out string name, out string lastname) =>
+        (name, lastname) = (Name, LastName);
+
+    public override string ToString() =>
+        $"{Name} {LastName}";
+}
---
+++ src/CSharp10/Deconstruct/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Deconstruct;
+
+var person = new Person("Andrei", "Fedotov");
+Console.WriteLine(person);
+
+(string name, string lastName) = person;
+Console.WriteLine(name);
+Console.WriteLine(lastName);
+
+var name2 = string.Empty;
+var lastname2 = string.Empty;
+(name2, lastname2) = person;
+Console.WriteLine(name2);
+Console.WriteLine(lastname2);
+
+Console.WriteLine("This is new C# 10 feature");
+Console.WriteLine("You can reuse existing variable and create new variables within the same decosntruct statement");
+
+var lastname3 = string.Empty;
+(var name3, lastname3) = person;
+Console.WriteLine(name3);
+Console.WriteLine(lastname3);
